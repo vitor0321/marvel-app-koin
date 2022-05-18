6d71a5d17712aebe990b28dcad387e421e1322f2
Vitor Walcker <vitor0321@hotmail.com>
2022-05-18 20:53:54 -0300

    configuração dos modulos do projeto

---
+++ app/src/main/java/com/example/marvelapp/MarvelApp.kt
@@ -0,0 +1,33 @@
+package com.example.marvelapp
+
+import android.app.Application
+import com.example.marvelapp.framework.di.navModule
+import com.example.marvelapp.framework.di.uiModule
+import com.example.marvelapp.framework.di.useCaseModule
+import com.example.marvelapp.framework.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
+
+class MarvelApp : Application( ) {
+
+    override fun onCreate() {
+        super.onCreate()
+        modulesKoin()
+    }
+
+    private fun modulesKoin() {
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MarvelApp)
+
+            modules(
+                navModule,
+                uiModule,
+                useCaseModule,
+                viewModelModule,
+            )
+        }
+    }
+}
